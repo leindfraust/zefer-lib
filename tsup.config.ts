a4e5ca7bc5b4cf27dd0614b0dc012abf3d668d11
Ronan <rozer223@gmail.com>
2024-02-07 20:25:39 +0800

    refactor: default splitting

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
     entry: ["src/index.ts"],
     format: ["esm", "cjs"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
-    splitting: false,
     sourcemap: true,
     clean: true,
 });
